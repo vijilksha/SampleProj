4d6ef115baf51ffe9c0c26fee28459628ca21aba
vijilksh2011@gmail.com <vijayalakshmi.v3@cognizant.com>
2023-05-18 17:26:16 +0530

    second commit

--- p1/src/com/cts/model/Departmenet.java
+++ p1/src/com/cts/model/Departmenet.java
@@ -1,5 +1,14 @@
 package com.cts.model;
 
 public class Departmenet {
+	
+	private int did;
+	private String dname;
+	public Departmenet(int did, String dname) {
+		super();
+		this.did = did;
+		this.dname = dname;
+	}
+	
 
 }
